5700869ea9164f45cba3f0df8b43468d4300d5da
albbus-stack <57916483+albbus-stack@users.noreply.github.com>
2020-09-21 12:38:23 +0200

    Update controller.h

--- controller.h
+++ controller.h
@@ -9,10 +9,15 @@ class Controller
 {
 public:
     Controller(List* l):list(l){}
+    
     void write(QString t,QString d,bool cS);
+    
     void clear();
+    
     QList<Item*> search(QString d);
+    
     void save(QString t,QString d,bool cS);
+    
 private:
     List* list;
 };
